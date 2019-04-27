49a9308d47f720651f797c7f1c546b0697b409be
kjirou <sorenariblog@gmail.com>
2019-04-27 14:01:04 +0900

    Define the routing logic as data

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ const ReactDOM = require('react-dom');
 
 const {UseStatePage} = require('./pages/UseStatePage');
 
+const routes = [
+  {
+    // TODO: Tmp
+    pathMatcher: /.*/,
+    //pathMatcher: /\/use-state\/?$/,
+    pageComponent: UseStatePage,
+  },
+];
+
+/**
+ * @param path {string} A root-relative path.
+ *                      Note that the so-called "public-url" is probably "/my-repo-name/" instead of "/".
+ */
+const findRouteFromPath = (routes, path) => {
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[i];
+    if (route.pathMatcher.test(path)) {
+      return route;
+    }
+  }
+
+  // TODO: 404
+};
+
 const App = () => {
-  return (
-    <UseStatePage />
-  );
+  const path = window.location.pathname;
+
+  const route = findRouteFromPath(routes, path);
+
+  return React.createElement(route.pageComponent);
 };
 
 window.addEventListener('DOMContentLoaded', () => {
