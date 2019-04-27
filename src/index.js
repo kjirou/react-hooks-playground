aab72c767e72b3023e64685e96f3fe2bebbaa9a6
kjirou <sorenariblog@gmail.com>
2019-04-27 13:02:53 +0900

    Write an example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const TextInputBoundToState = (props) => {
+  const {
+    advice,
+    handleChange,
+    inputValue,
+  } = props;
+
+  return (
+    <div>
+      <h3>Bound to the state</h3>
+      <div>
+        <input type="text" value={props.inputValue} onChange={handleChange} />
+        <span>Remining {advice} characters.</span>
+      </div>
+    </div>
+  );
+};
+
+const initialState = {
+  inputValue: '',
+  maxLength: 5,
+};
+
+const mapStateToProps = (state, setState) => {
+  return {
+    advice: `${state.maxLength - state.inputValue.length}/${state.maxLength}`,
+    handleChange: (event) => {
+      const inputValue = event.target.value;
+      setState(state => {
+        return Object.assign({}, state, {inputValue});
+      });
+    },
+    inputValue: state.inputValue,
+  };
+};
+
 const App = () => {
-  return <div>Hello, React app!</div>;
+  const [state, setState] = React.useState(initialState);
+
+  const props = mapStateToProps(state, setState);
+
+  return (
+    <TextInputBoundToState
+      advice={props.advice}
+      handleChange={props.handleChange}
+      inputValue={props.inputValue}
+    />
+  );
 };
 
 window.addEventListener('DOMContentLoaded', () => {
